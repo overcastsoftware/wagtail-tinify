5af32bc334459fb05984b5d5bdebad945178bcea
Sævar Öfjörð Magnússon <saevar@saevar.is>
2017-01-02 13:08:23 +0000

    Use cloudflare credentials

--- wagtail_tinify/models.py
+++ wagtail_tinify/models.py
@@ -10,7 +10,9 @@ aws_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret = os.getenv('AWS_SECRET_ACCESS_KEY')
 aws_bucket = os.getenv('AWS_STORAGE_BUCKET_NAME')
 aws_region = os.getenv('AWS_REGION')
-cf_key = os.getenv('CF_API_KEY')
+cf_api_key = os.getenv('CF_API_KEY')
+cf_api_email = os.getenv('CF_API_EMAIL')
+cf_zone_id = os.getenv('CF_ZONE_ID')
 tinify.key = os.getenv('TINYPNG_API_KEY')
 
 # If cloudflare key is not set, then we don't need to import Cloudflare
@@ -31,9 +33,9 @@ class TinyPngOptimizeThread(threading.Thread):
                 source = tinify.from_url(source_url_https)
                 path = "%s/%s" % (aws_bucket, self.instance.file.name)
                 source.store(service='s3',aws_access_key_id=aws_key_id,aws_secret_access_key=aws_secret,region=aws_region,path=path)
-                if cf_key:
+                if cf_zone_id and cf_api_key and cf_api_email:
                     cf = CloudFlare.CloudFlare()
-                    cf.zones.purge_cache.delete(cf_key, data={'files':[source_url_http, source_url_https]})
+                    cf.zones.purge_cache.delete(cf_zone_id, data={'files':[source_url_http, source_url_https]})
             # Else we grab the local image, optimize it and override the local file
             else:
                 path = os.getcwd()+self.instance.url
